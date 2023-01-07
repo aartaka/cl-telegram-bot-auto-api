acdb15633b8fbcecfc2fbabe9abed05b1c8eeb64
aartaka <aartaka@protonmail.com>
2023-01-07 22:08:04 +0400

    Export stop.

--- cl-telegram-bot-auto-api.lisp
+++ cl-telegram-bot-auto-api.lisp
@@ -254,7 +254,7 @@
       (when-apply on-chat-join-request chat-join-request)))
   (:documentation "Process the parts of the update (if present), be it message, chat join request, or whatever."))
 
-(serapeum:export-always 'start)
+(serapeum:export-always '(start stop))
 (defvar *thread* nil)
 (let (*thread*)
   (defun start (token &key update-callback (timeout 10))
