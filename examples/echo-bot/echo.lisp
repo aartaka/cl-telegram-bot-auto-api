04aa07e3fde60f489b9aec567f7d1c55e13c5af6
Artyom Bologov <aartaka@protonmail.com>
2023-01-14 16:01:25 +0400

    examples/echo-bot/echo: Add error handler.

--- examples/echo-bot/echo.lisp
+++ examples/echo-bot/echo.lisp
@@ -4,5 +4,10 @@
   "Respond to the message with the same text it contains (actually using `tga:copy-message')."
   (tga:copy-message (tga:chat message) (tga:chat message) message))
 
+(defmethod tga:on ((object error))
+  "Invoke CONTINUE restart of OBJECT, if found."
+  (when (find-restart 'continue object)
+    (invoke-restart 'continue)))
+
 (defun entry-point (&key (token (first uiop:*command-line-arguments*)))
   (tga:start token :name "Echo bot"))
