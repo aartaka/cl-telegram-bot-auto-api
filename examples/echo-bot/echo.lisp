dc443866b7ae328f1079234bd247bdbcb9588596
Artyom Bologov <aartaka@protonmail.com>
2023-02-06 22:48:18 +0400

    examples/echo-bot/echo: Better error handler.

--- examples/echo-bot/echo.lisp
+++ examples/echo-bot/echo.lisp
@@ -5,9 +5,12 @@
   (tga:copy-message (tga:chat message) (tga:chat message) message))
 
 (defmethod tga:on ((object error))
-  "Invoke CONTINUE restart of OBJECT, if found."
-  (when (find-restart 'continue object)
-    (invoke-restart 'continue)))
+  "Invoke CONTINUE restart of OBJECT, if found.
+Otherwise print the backtrace and return."
+  (or (continue)
+      (progn
+        (format t "~a" object)
+        (uiop:print-backtrace :condition object))))
 
 (defun entry-point (&key (token (first uiop:*command-line-arguments*)))
   (tga:start token :name "Echo bot"))
