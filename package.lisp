436925d1f71274c7fc6d91a87ab4fae93e83e194
Artyom Bologov <git@aartaka.me>
2025-04-30 01:25:46 +0400

    package: Use uninterned symbols.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,4 @@
 (uiop:define-package #:cl-telegram-bot-auto-api
   (:nicknames #:tga #:tg-bot-auto)
-  (:shadow :type :length :close :position :quote :fill :format :number :symbol)
+  (:shadow #:type #:length #:close #:position #:quote #:fill #:format #:number #:symbol)
   (:use #:cl))
